a2285eda28c89e0f2e4d8d4d17bd3ae71b26f629
Saima Sharleen Islam <saimasharleen@users.noreply.github.com>
2023-01-31 12:24:50 +0100

    Update utility_matrix.py

--- utility_matrix.py
+++ utility_matrix.py
@@ -1,17 +1,34 @@
+import random
 import csv
+import uuid
 
-# Create a list of query ids
-query_ids = ["Q1821", "Q1822", "Q1823", "Q1824", "Q1825", "Q1826"]
+# create the set of user IDs
+user_ids = set()
+while len(user_ids) < 10000:
+    user_ids.add(str(uuid.uuid4()))
+    
+# create the set of query IDs
+query_ids = set()
+while len(query_ids) < 5000:
+    query_ids.add("Q"+str(uuid.uuid4()))
 
-# Create a list of rows for the utility matrix
-rows = [
-    ["U_11", 3, "", 88, 3, 95, 2],
-    ["U_12", 12, 50, 90, 2, 77, 10],
-    ["U_13", "", "", "", 95, 1, 24],
-]
+# create the utility matrix
+utility_matrix = []
 
-# Write the query ids and rows to a CSV file
-with open("utility_matrix.csv", "w", newline="") as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(query_ids)
-    writer.writerows(rows)
+# add the first row (query IDs)
+utility_matrix.append(list(query_ids))
+
+# add the rest of the rows (user IDs and random values)
+for user_id in user_ids:
+    row = [user_id]
+    for query_id in query_ids:
+        if random.random() < 0.8:
+            row.append(random.randint(1,100))
+        else:
+            row.append('')
+    utility_matrix.append(row)
+
+# write the utility matrix to a CSV file
+with open('utility_matrix.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(utility_matrix)
