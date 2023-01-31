69dabe56dfaedb347f148a8c63545897dd0ee33b
Saima Sharleen Islam <saimasharleen@users.noreply.github.com>
2023-01-31 12:25:37 +0100

    Update user.py

--- user.py
+++ user.py
@@ -1,15 +1,14 @@
 import csv
+import uuid
+import pandas as pd
 
-# Define the user ids
-user_ids = ["U_1", "U_2", "U_3", "U_4", "U_5", "U_6", "U_7", "U_8", "U_9", "U_10", "U_11", "U_12", "U_13", "U_14", "U_15", "U_16", "U_17", "U_18", "U_19", "U_20"]
+user_ids = set()
+while len(user_ids) < 10000:
+    user_ids.add(str(uuid.uuid4()))
 
-# Create a list of dictionaries representing the rows of the CSV file
-rows = []
-for user_id in user_ids:
-    rows.append({"user_id": user_id})
+# convert the set to dataframe
+user_ids_df = pd.DataFrame(list(user_ids), columns=["user_id"])
 
-# Write the rows to a CSV file
-with open("user_ids.csv", "w", newline="") as f:
-    writer = csv.DictWriter(f, fieldnames=["user_id"])
-    writer.writeheader()
-    writer.writerows(rows)
+# write the dataframe to a csv file
+user_ids_df.to_csv("user_ids.csv", index=False)
+
